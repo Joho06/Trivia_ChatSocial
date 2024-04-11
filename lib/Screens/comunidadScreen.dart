f59f61042189a1f8a27a1a77dc462909295866b8
Joho06 <jhjaviersartica@gmail.com>
2024-04-11 15:17:12 -0500

    database

--- lib/Screens/comunidadScreen.dart
+++ lib/Screens/comunidadScreen.dart
@@ -1,6 +1,5 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 class ComunidadScreen extends StatefulWidget {
   const ComunidadScreen({Key? key}) : super(key: key);
@@ -11,13 +10,6 @@ class ComunidadScreen extends StatefulWidget {
 
 class _ComunidadScreenState extends State<ComunidadScreen> {
   List<bool> likes = [false, false, false, false]; // Estado de "me gusta" para cada publicación
-  List<List<String>> comentarios = [
-    [],
-    [],
-    [],
-    [],
-  ]; // Lista de comentarios para cada publicación
-
   List<String> nombres = [
     'Michael Graves',
     'Vince Neil',
@@ -37,13 +29,6 @@ class _ComunidadScreenState extends State<ComunidadScreen> {
     'assets/images/publish/img2.jpg',
     'assets/images/publish/img3.jpg',
     'assets/images/publish/img4.jpg',
-  ];
-
-  List<String> comentarioPublicacion = [
-    'De vacaciones',
-    'Relajado',
-    'Descansando...',
-    'Feliz...',
   ];
 
   TextEditingController comentarioController = TextEditingController();
@@ -105,25 +90,15 @@ class _ComunidadScreenState extends State<ComunidadScreen> {
                       IconButton(
                         icon: const Icon(Icons.comment_outlined),
                         onPressed: () {
-                          __agregarComentario(index);
+                          _agregarComentario(context, index);
                         },
                       ),
                     ],
                   ),
                 ),
                 const SizedBox(height: 10),
-                Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 20),
-                  child: Text(
-                    comentarioPublicacion[index],
-                    style: const TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
-                  ),
-                ),
-
                 const Divider(),
-                Column(
-                  children: _buildComentarios(index),
-                ),
+                _buildComentarios(index),
               ],
             ),
           );
@@ -132,55 +107,55 @@ class _ComunidadScreenState extends State<ComunidadScreen> {
     );
   }
 
-  List<Widget> _buildComentarios(int index) {
-    List<Widget> listaComentarios = [];
-    for (int i = 0; i < comentarios[index].length; i++) {
-      String comentario = comentarios[index][i];
-      listaComentarios.add(
-        Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 20.0, vertical: 8.0),
-          child: Row(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              CircleAvatar(
-                radius: 20,
-                backgroundImage: AssetImage(imagenesUsuario[index]),
-              ),
-              const SizedBox(width: 10),
-              Expanded(
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text(
-                      nombres[index],
-                      style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+  Widget _buildComentarios(int index) {
+    return StreamBuilder<QuerySnapshot>(
+      stream: FirebaseFirestore.instance
+          .collection('comentarios')
+          .where('index', isEqualTo: index.toString())
+          .snapshots(),
+      builder: (context, snapshot) {
+        if (!snapshot.hasData) {
+          return Container();
+        }
+        var comentarios = snapshot.data!.docs;
+        return Column(
+          children: comentarios.map<Widget>((comentario) {
+            return Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 20.0, vertical: 8.0),
+              child: Row(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  CircleAvatar(
+                    radius: 20,
+                    backgroundImage: AssetImage(imagenesUsuario[index]),
+                  ),
+                  const SizedBox(width: 10),
+                  Expanded(
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          nombres[index],
+                          style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+                        ),
+                        const SizedBox(height: 4),
+                        Text(
+                          comentario['comentario'],
+                          style: const TextStyle(fontSize: 14),
+                        ),
+                      ],
                     ),
-                    const SizedBox(height: 4),
-                    Text(
-                      comentario,
-                      style: const TextStyle(fontSize: 14),
-                    ),
-                  ],
-                ),
+                  ),
+                ],
               ),
-              IconButton(
-                icon: const Icon(Icons.delete),
-                onPressed: () {
-                  setState(() {
-                    comentarios[index].removeAt(i);
-                  });
-                },
-              ),
-            ],
-          ),
-        ),
-      );
-      listaComentarios.add(const Divider());
-    }
-    return listaComentarios;
+            );
+          }).toList(),
+        );
+      },
+    );
   }
 
-  __agregarComentario(int index) {
+  void _agregarComentario(BuildContext context, int index) {
     showModalBottomSheet(
       context: context,
       builder: (BuildContext context) {
@@ -214,11 +189,8 @@ class _ComunidadScreenState extends State<ComunidadScreen> {
                       onPressed: () {
                         String comentarioText = comentarioController.text.trim();
                         if (comentarioText.isNotEmpty) {
-                          saveComentarioToFirebase(comentarioText, 'like', index);
-                          setState(() {
-                            comentarios[index].add(comentarioText);
-                            comentarioController.clear();
-                          });
+                          _guardarComentario(comentarioText, index);
+                          comentarioController.clear();
                           Navigator.of(context).pop();
                         } else {
                           ScaffoldMessenger.of(context).showSnackBar(
@@ -240,11 +212,11 @@ class _ComunidadScreenState extends State<ComunidadScreen> {
     );
   }
 
-  Future<void> saveComentarioToFirebase(String comentario, String reaccion, int index) async {
+  Future<void> _guardarComentario(String comentario, int index) async {
     try {
-      await FirebaseFirestore.instance.collection('comentarios').doc().set({
+      await FirebaseFirestore.instance.collection('comentarios').add({
         'comentario': comentario,
-        'reaccion': reaccion,
+        'index': index.toString(), // Agregar el índice de la publicación para identificarla
       });
     } catch (error) {
       print('Error al guardar el comentario en Firestore: $error');
